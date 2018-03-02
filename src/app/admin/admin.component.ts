ef48f237a63793676bdbc3146075b5f3676c845e
Guest User <Guest@Epicodus-4A.local>
2018-03-01 16:41:23 -0800

    cleanup photo upload function

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -24,15 +24,14 @@ export class AdminComponent implements OnInit {
     let name = file.name;
     const storageRef: firebase.storage.Reference = firebase.storage().ref('/photos/' + name)
     const uploadTask: firebase.storage.UploadTask = storageRef.put(file, metaData);
-
     uploadTask.then((uploadSnapshot: firebase.storage.UploadTaskSnapshot) => {
       firebase.database().ref('/photos/urls/').push(uploadSnapshot.downloadURL);
     })
    }
 
 
-  submitForm(projectName: string, managerName: string, goal: number, productDescription: string, rewards: string) {
-    var newProject: Project = new Project(projectName, managerName, goal, productDescription, rewards);
+  submitForm(projectName: string, managerName: string, goal: number, productDescription: string, rewards: string, image: string) {
+    var newProject: Project = new Project(projectName, managerName, goal, productDescription, rewards, image);
     this.projectService.addProjects(newProject);
   }
 }
--- src/app/project.model.ts
+++ src/app/project.model.ts
@@ -1,3 +1,3 @@
 export class Project {
-  constructor(public projectName: string, public managerName: string, public goal: number, public productDescription: string, public rewards: string) {}
+  constructor(public projectName: string, public managerName: string, public goal: number, public productDescription: string, public rewards: string, public image: string) {}
 }
