9811c24fbeaa657b566d582e450a2687254b6308
Matthew Jackson <31641478+madjacks12@users.noreply.github.com>
2018-04-12 11:41:04 -0700

    Update api-key.ts

--- src/app/api-key.ts
+++ src/app/api-key.ts
@@ -1,8 +1,8 @@
 export var masterFirebaseConfig = {
-  apiKey: "AIzaSyD84wfXLi5L_Qy_fwRZZVdI532NtkXe5AY",
-  authDomain: "crowdfunding-e34c8.firebaseapp.com",
-  databaseURL: "https://crowdfunding-e34c8.firebaseio.com",
-  projectId: "crowdfunding-e34c8",
-  storageBucket: "crowdfunding-e34c8.appspot.com",
-  messagingSenderId: "552953994639"
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
